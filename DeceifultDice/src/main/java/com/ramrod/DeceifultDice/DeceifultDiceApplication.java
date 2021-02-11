14a308f3a17963dbaf64e6147372586702217b2a
rajsukanya <sukanyaraj1014@yahoo.com>
2021-02-10 19:02:46 -0500

    spring project created

---
+++ DeceifultDice/src/main/java/com/ramrod/DeceifultDice/DeceifultDiceApplication.java
@@ -0,0 +1,13 @@
+package com.ramrod.DeceifultDice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeceifultDiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeceifultDiceApplication.class, args);
+	}
+
+}
---
+++ DeceifultDice/src/test/java/com/ramrod/DeceifultDice/DeceifultDiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramrod.DeceifultDice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeceifultDiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
